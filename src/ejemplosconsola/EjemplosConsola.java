4766b7f4722e2b0ae6c7c586915752ce64dbeac7
Jorge Cisneros <fraxito@gmail.com>
2019-01-18 12:50:05 +0100

    Versión 2 del programa que calcula los máximos. El bucle FOR ahora empieza en 1 en vez de en 0 (y funciona)

--- src/ejemplosconsola/EjemplosConsola.java
+++ src/ejemplosconsola/EjemplosConsola.java
@@ -25,25 +25,24 @@ public class EjemplosConsola {
         //declaramos un array de dos elementos para guardar el máximo y el segundo máximo
         int [] listaMaximos = {lista[0], lista[1]};
         
-        for (int i=0; i< lista.length; i++){
+        //empiezo a comparar desde la posicion 1, no desde la 0
+        for (int i=1; i< lista.length; i++){
             if ( lista[i] >= listaMaximos[0]){
                 //si llega aquí, es que el número que estoy 
-                //comparando es mayor o igual que el que tengo primero en la 
-                //lista de máximos
-                if (i!=0) { //desplazo sólo a la derecha si no estoy justo en la primera posición
-                    listaMaximos[1] = listaMaximos[0]; //desplazo a la derecha el que había como máximo
-                } 
+                //comparando es mayor o igual que el que tengo primero en la lista de máximos
+                listaMaximos[1] = listaMaximos[0]; //desplazo a la derecha el que había como máximo
                 listaMaximos[0] = lista[i];  //pongo en la primera posición al nuevo máximo
             }
-            else  if ( lista[i] >= listaMaximos[1]){ //compruebo si el número que estoy leyendo es mayor que 
-                                                     //el SEGUNDO de la lista de máximos
+            else  if ( lista[i] >= listaMaximos[1]){ //compruebo si el número que estoy leyendo es mayor que                                                      //el SEGUNDO de la lista de máximos
                 listaMaximos[1] = lista[i];  //pongo en la SEGUNDA posición al nuevo SEGUNDO máximo
             }
-        }
-        
+        }       
         return listaMaximos;
     }
-                 
+    
+
+    
+    
     /**
      * @param args the command line arguments
      */
